2c37b92976f8181cde1771d04704edcfe80e0935
fan240579805 <240579805@qq.com>
2021-06-24 15:40:09 +0800

    feat: 分享

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <SwitchDemo />
+  <div class="bg-red-200">asdsad vite</div>
+  <elementDemo/>
+</template>
+
+<script setup>
+import SwitchDemo from "./components/SwitchDemo.vue";
+import elementDemo from "./components/elementDemo.vue";
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+.elg{
+  background-color: #eee !important;
+}
+</style>
---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,32 @@
+<template>
+  <SwitchGroup>
+    <div class="flex items-center">
+      <SwitchLabel class="mr-4">Enable notifications</SwitchLabel>
+      <Switch
+        v-model="enabled"
+        :class="enabled ? 'bg-blue-600' : 'bg-gray-200'"
+        class="relative inline-flex items-center h-6 transition-colors rounded-full w-11 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      > 
+        <span
+          :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+          class="inline-block w-8 h-8 transition-transform transform bg-white rounded-full"
+        />
+      </Switch>
+    </div>
+  </SwitchGroup>
+</template>
+
+<script>
+import { ref } from "vue";
+import { Switch, SwitchGroup, SwitchLabel } from "@headlessui/vue";
+
+export default {
+  components: { Switch, SwitchGroup, SwitchLabel },
+
+  setup() {
+    const enabled = ref(false);
+
+    return { enabled };
+  },
+};
+</script>
